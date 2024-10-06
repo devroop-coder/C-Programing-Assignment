0b4713aef69274b0bd9674e3fc491bdb99fc2cc3
Kaushik Ranjan <kaushikranjanrajkumar@gmail.com>
2024-10-06 20:31:03 +0530

    6 oct 2024

---
+++ profitLoss.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    
+    printf("Kaushik Ranjan Rajkumar\n");
+
+    int cp,sp,res;
+    printf("Enter the cost price: ");
+    scanf("%d",&cp);
+    printf("Enter the selling price: ");
+    scanf("%d",&sp);
+
+    if (cp > sp)
+    {
+        res = cp - sp;
+        printf("Loss = %d\n", res);
+    }else if(cp < sp)
+    {
+        res = sp - cp;
+        printf("Profit = %d\n", res);
+    }else
+    {
+        printf("No profit or loss\n");
+    }
+    
+
+    return 0;
+}
